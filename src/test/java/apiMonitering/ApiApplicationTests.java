939473cc4edc419955459e5bc0868bfc0396293b
RapiL <sjw3090@gmail.com>
2024-11-03 18:37:17 +0900

    1

---
+++ src/test/java/apiMonitering/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package apiMonitering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
