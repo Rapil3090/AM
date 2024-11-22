6296672dcc16ca422503b2065d526da22a750339
Rapil <120554681+Rapil3090@users.noreply.github.com>
2024-11-22 17:46:32 +0900

    에러메세지 추가

--- src/main/java/apiMonitering/type/ErrorCode.java
+++ src/main/java/apiMonitering/type/ErrorCode.java
@@ -10,6 +10,9 @@ public enum ErrorCode {
     ENDPOINT_ID_NOT_FOUND("저장된 API_ID가 없습니다."),
     INVALID_SERVICEKEY("잘못된 서비스키입니다."),
     INVALID_REQUEST("잘못된 요청입니다."),
+    CLIENT_ERROR("클라이언트 요청 오류"),
+    SERVER_ERROR("서버 오류 발생"),
+    UNKNOWN_ERROR("알 수 없는 오류 발생"),
     INTERNAL_SERVER_ERROR("내부 서버 오류가 발생했습니다.");
 
     private final String Description;
