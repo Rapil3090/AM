dce496b883a6189d86a7bb16807ce2c618e4254f
RapiL <sjw3090@gmail.com>
2024-11-26 23:23:15 +0900

    마지막 레포지토리 저장로직 수정

--- src/main/java/apiMonitering/handler/ApiResponseHandler.java
+++ src/main/java/apiMonitering/handler/ApiResponseHandler.java
@@ -26,32 +26,44 @@ public class ApiResponseHandler {
         apiResponse.setResponseTime(responseTime);
         apiResponse.set_success(false);
 
+        if (throwable instanceof TimeoutException timeout) {
+
+            apiResponse.setStatusCode(408);
+            apiResponse.setErrorMessage("타임아웃");
+            log.error("타임아웃 발생 : {}", timeout.getMessage());
+
+            return Mono.just(apiResponseRepository.save(apiResponse));
+        }
+
         if (throwable instanceof WebClientResponseException error) {
             int statusCode = error.getStatusCode().value();
             apiResponse.setStatusCode(statusCode);
             apiResponse.setErrorMessage(error.getMessage());
 
+            if (statusCode == 408) {
+                log.error("타임아웃 발생 : 상태코드 = {}, 메시지 = {}", statusCode, error.getMessage());
+                throw new ApiEndPointException(ErrorCode.TIMEOUT_ERROR);
+            }
+
+            if (statusCode == 500) {
+                log.error("서버 내부 오류 발생 : 상태코드 = {}, 메시지 = {}", statusCode, error.getMessage());
+                throw new ApiEndPointException(ErrorCode.SERVER_ERROR);
+            }
+
             if (error.getStatusCode().is4xxClientError()) {
                 log.error("4xx 클라이언트 에러 발생 : 상태코드 = {}, 메시지 = {}", statusCode, error.getMessage());
                 throw new ApiEndPointException(ErrorCode.CLIENT_ERROR, "클라이언트 오류 : " + error.getMessage());
-            } else if (error.getStatusCode().is5xxServerError()) {
+            }
+
+            if (error.getStatusCode().is5xxServerError()) {
                 log.error("5xx 서버 에러 발생 : 상태코드 = {}, 메시지 = {}", statusCode, error.getMessage());
                 throw new ApiEndPointException(ErrorCode.SERVER_ERROR, "서버 오류 : " + error.getMessage());
             }
-        } else if (throwable instanceof TimeoutException timeout) {
-
-            apiResponse.setStatusCode(408);
-            apiResponse.setErrorMessage("타임아웃");
-            log.error("타임아웃 발생 : {}", timeout.getMessage());
-            throw new ApiEndPointException(ErrorCode.TIMEOUT_ERROR);
-        } else {
-
-            apiResponse.setStatusCode(500);
-            apiResponse.setErrorMessage("알 수 없는 에러 발생");
-            log.error("알 수 없는 에러 발생 : {}", throwable.getMessage());
-            throw new ApiEndPointException(ErrorCode.UNKNOWN_ERROR);
-
         }
+
+        apiResponse.setStatusCode(500);
+        apiResponse.setErrorMessage("알 수 없는 에러 발생");
+        log.error("알 수 없는 에러 발생 : {}", throwable.getMessage());
 
         return Mono.just(apiResponseRepository.save(apiResponse));
     }
