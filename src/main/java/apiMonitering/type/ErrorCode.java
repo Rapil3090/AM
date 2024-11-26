baeb89c9a5cff235cca10195b35f5da92db9405a
RapiL <sjw3090@gmail.com>
2024-11-26 22:59:32 +0900

    타임아웃 추가

--- src/main/java/apiMonitering/type/ErrorCode.java
+++ src/main/java/apiMonitering/type/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
     SERVER_ERROR("서버 오류 발생"),
     UNKNOWN_ERROR("알 수 없는 오류 발생"),
     RETRY_EXHAUSTED("재시도 횟수를 모두 소모했습니다."),
+    TIMEOUT_ERROR("요청시간이 초과되었습니다"),
     INTERNAL_SERVER_ERROR("내부 서버 오류가 발생했습니다.");
 
     private final String Description;
